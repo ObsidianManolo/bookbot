aec6b1a9ac12ca744b920703028bee14cb66c365
ObsidianManolo <office.manolo@protonmail.com>
2024-09-18 19:47:17 +0200

    Nouvelle fonction pour générer un rapport

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ def main():
 
     text = get_book_text(file)
     wordcount = count_words(text)
+    charcount = count_chars(text)
 
-    #dev : test fonctions
-    count_chars(text)
+    # Affichage final
+    create_report(wordcount,charcount,file)
 
 
-    # Message final
-    #print(f"[1] Il y a {wordcount} mots dans ce texte")
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,16 +21,25 @@ def count_words(file_content):
 def count_chars(file_content):
     # Passe la string en minuscule pour éviter les doublons
     file_tolowercase = file_content.lower()
-
     chars_count_dic = {} # dictionnaire pour stocker le compte des lettres
 
     for char in file_tolowercase:
-        if char not in chars_count_dic:
-            chars_count_dic[char] = 1
-        else:
-            chars_count_dic[char] += 1
+        if char.isalpha():
+            if char not in chars_count_dic:
+                chars_count_dic[char] = 1
+            else:
+                chars_count_dic[char] += 1
     
     return chars_count_dic
+
+def create_report(wordcount,charcount,file):
+    print(f"--- Begin report of {file} ---")
+    print(f"{wordcount} words found in this document")
+
+    for char in charcount.keys():
+        print(f"The {char} character was found {charcount[char]} times")
+    print("--- End Report ---")
+
         
 
     
